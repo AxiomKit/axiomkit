e5f3214c9cdced7e560e72aefa2f3dc7bfa24315
neon-core <neonworking00@gmail.com>
2025-07-23 15:10:39 +0700

    chore: update cli packages

--- packages/sei/src/sei.ts
+++ packages/sei/src/sei.ts
@@ -1,13 +1,8 @@
 import * as viemChains from "viem/chains";
 
-import { createPublicClient, http, type Abi, type PublicClient } from "viem";
+import { createPublicClient, http, type PublicClient } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
-export const seiChains: Record<string, viemChains.Chain> = {
-  mainnet: viemChains.sei,
-  testnet: viemChains.seiTestnet,
-  devnet: viemChains.seiDevnet,
-};
-export type SeiChainName = keyof typeof seiChains;
+
 export interface SeiChainConfig {
   chain: viemChains.Chain;
   rpcUrl: string;
@@ -26,35 +21,8 @@ export class SeiChain {
       transport: http(),
     });
 
-    console.log("What Wrong with Signer", config.privateKey);
     this.signer = privateKeyToAccount(config.privateKey);
   }
-
-  // public async readContract(call: unknown) {
-  //   const { functionName, address, abi, args } = call as {
-  //     functionName: string;
-  //     address: `0x${string}`;
-  //     abi: Abi;
-  //     args?: any[];
-  //   };
-
-  //   const data = await this.client.readContract({
-  //     address: address,
-  //     abi: abi,
-  //     functionName: functionName,
-  //     args: args ? [...args] : [],
-  //   });
-  //   return data;
-  // }
-
-  // public async writeContract(call: unknown) {
-  //   const { functionName, address, abi, args } = call as {
-  //     functionName: string;
-  //     address: `0x${string}`;
-  //     abi: Abi;
-  //     args: any[];
-  //   };
-  // }
 
   public getClient() {
     return this.client;
