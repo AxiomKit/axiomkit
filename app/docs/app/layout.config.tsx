4f1c460b1c9a28215bea11fb07ef013c05ed1540
sujonghell <sujongworking@gmail.com>
2025-08-18 16:41:11 +0700

    upgrade: docs context

--- app/docs/app/layout.config.tsx
+++ app/docs/app/layout.config.tsx
@@ -1,5 +1,6 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
-
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import Image from "next/image";
+import { FaGithub } from "react-icons/fa";
 /**
  * Shared layout configurations
  *
@@ -11,18 +12,26 @@ export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
-        My App
+        <Image
+          src={`/logo.png`}
+          alt="Logo Axiom"
+          height={32}
+          width={32}
+          className="w-auto"
+        />
+        <span className="text-red-600">AxiomKit</span>
       </>
     ),
   },
   // see https://fumadocs.dev/docs/ui/navigation/links
-  links: [],
+  links: [
+    {
+      type: "icon",
+      icon: <FaGithub />,
+      label: "Github",
+      text: "Github",
+      url: "https://github.com/AxiomKit/axiomkit",
+      external: true,
+    },
+  ],
 };
--- app/docs/app/layout.tsx
+++ app/docs/app/layout.tsx
@@ -1,12 +1,56 @@
-import '@/app/global.css';
-import { RootProvider } from 'fumadocs-ui/provider';
-import { Inter } from 'next/font/google';
-import type { ReactNode } from 'react';
+import "@/app/global.css";
+import { RootProvider } from "fumadocs-ui/provider";
+import { Inter } from "next/font/google";
+import type { ReactNode } from "react";
+import Favicon from "./favicon.ico";
+import Banner from "@/public/banner.png";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://axiomkit.altlab.space"),
+  title: "AxiomKit - Intelligent Agent Framework",
+  description:
+    "Build agents that think, learn, and evolve with AxiomKit's modular TypeScript framework",
+  applicationName: "Axiomkit",
+  icons: {
+    icon: Favicon.src,
+    shortcut: Favicon.src,
+    apple: Favicon.src,
+    other: { rel: "apple-touch-icon-precomposed", url: Favicon.src },
+  },
+  keywords: ["Axiomkit"],
+  openGraph: {
+    title: "AxiomKit - Intelligent Agent Framework",
+    description:
+      "Build agents that think, learn, and evolve with AxiomKit's modular TypeScript framework",
+    images: [
+      {
+        url: Banner.src,
+        width: 1344,
+        height: 896,
+        type: "image/png",
+      },
+    ],
+    locale: "en_US",
+    url: "https://axiomkit.altlab.space",
+    type: "website",
+    emails: "neonworking00@gmail.com",
+    siteName: "AxiomKit - Intelligent Agent Framework",
+  },
+  twitter: {
+    title: "AxiomKit - Intelligent Agent Framework",
+    description:
+      "Build agents that think, learn, and evolve with AxiomKit's modular TypeScript framework",
+    images: Banner.src,
+    site: "https://axiomkit.altlab.space",
+    creator: "@axiomkit",
+  },
+  category: "technology",
+};
 
 const inter = Inter({
-  subsets: ['latin'],
+  subsets: ["latin"],
 });
-
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
